79d2581adc9a693e44943fee4565bd2d2e75b5f0
18875014823 <2499126407@qq.com>
2017-05-04 16:44:27 +0800

    单车一览

--- Application/Home/Controller/BikeController.class.php
+++ Application/Home/Controller/BikeController.class.php
@@ -1,8 +1,33 @@
 <?php
-namespace Application\Home\Controller;
+namespace Home\Controller;
 
-class BikeController{
+use Think\Controller;
+use Think\Model;
+
+class BikeController extends Controller{
+    private $bikeModel;
     
+    /**
+     * 构造方法
+     */
+    public function __construct(){
+        parent::__construct();
+        $this->bikeModel = new Model(tb_bike);
+    }
+    
+    public function loadBikeByPage($pageNo = 1,$pageSize = 10){
+        $total = $this->bikeModel->count();
+        
+        //->fetchSql(true)
+        $rows = $this->bikeModel->page($pageNo,$pageSize)->select();
+        $page = array("total"=>$total, "rows"=>$rows, "pageNo"=>$pageNo, "pageSize"=>$pageSize);
+        
+        $this->assign("page",$page);
+
+        $this->assign("BASEPATH",BASEPATH);
+//         print_r($page);
+        $this->display("bikeInformationList");
+    }
 }
 
 ?>
