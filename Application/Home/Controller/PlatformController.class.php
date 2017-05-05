c90d7eee9b370a2c07dd95d6d2ec2e961bef0c1a
廖斌 <1344551696@qq.com>
2017-05-05 17:31:05 +0800

    liaobin

--- Application/Home/Controller/PlatformController.class.php
+++ Application/Home/Controller/PlatformController.class.php
@@ -24,6 +24,58 @@ class PlatformController extends Controller{
         $this->assign("result",$result);
        // $this->display("");//跳转页面
     }
+    /**
+     * 同步提交增加或者修改平台
+     */
+    function addOrUpdate(){
+        $data=$this->platformMode->create();
+        if($data["pId"] < 0){//增加客户信息
+            $this->platformMode->field("pPosition,pIsUsed")->add($data);
+    
+        }else{//修改客户信息
+            $this->platformMode->field("pPosition,pIsUsed")->where("pId='%d'",$data["pId"])->save();
+        }
+        $this->loadPlatform();
+    }
+    
+    /**
+     * 分页加载平台信息
+     */
+    public function loadPlatform($pageNo=1,$pageSize=2,$pId=0){
+        if($pId==0){//$pId没传值
+            //总数量
+            $total=$this->platformMode->count();
+            //当前页的数据
+            $rows=$this->platformMode->page($pageNo,$pageSize)->order("pId desc")->select();
+            $page=array("total"=>$total,"rows"=>$rows,"pageNo"=>$pageNo,"pageSize"=>$pageSize);
+            $this->assign("BASEPATH",BASEPATH);//保存数据方便页面使用
+            $this->assign("Platforms",$page);//保存数据方便页面使用
+            $this->display("platformShow"); 
+        }else {//$pId传值通过id查询一条数据
+            $data=$this->platformMode->find($pId);
+            $this->ajaxReturn($data);
+        }
+    }
+    
+    /**
+     * 收索平台
+     */
+    public function searchPlatform($pageNo=1,$pageSize=2,$pPosition){
+       
+        $query="1=1 ";
+        if($pPosition!=null && $pPosition !=""){
+            $query .="and pPosition LIKE '%$pPosition%'";
+        }
+        //总数量
+        $total=$this->platformMode->where($query)->count();
+        //当前页的数据
+        $rows=$this->platformMode->where($query)->page($pageNo,$pageSize)->order("pId desc")->select();
+        $page=array("total"=>$total,"rows"=>$rows,"pageNo"=>$pageNo,"pageSize"=>$pageSize);
+        
+        $this->assign("BASEPATH",BASEPATH);
+        $this->assign("Platforms",$page);//保存数据方便页面使用
+        $this->display("platformShow"); 
+    }
 }
 
 ?>
